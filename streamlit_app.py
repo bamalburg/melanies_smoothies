854cd859cdff3d8cdd3cfea20dd029c380056a6b
Ben Malburg <46354299+bamalburg@users.noreply.github.com>
2024-07-14 17:50:13 -0400

    removing unnecessary messages

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,9 +18,8 @@ st.write("The name on your Smoothie will be: ", name_on_order)
 cnx = st.connection(name="snowflake")
 session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'), col('SEARCH_ON'))
-# st.dataframe(data=my_dataframe, use_container_width=True)
 pd_df = my_dataframe.to_pandas()
-st.dataframe(data=pd_df, use_container_width=True)
+# st.dataframe(data=pd_df, use_container_width=True)
 # st.stop()
 
 ingredients_list = st.multiselect(
@@ -34,7 +33,7 @@ if ingredients_list:
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
         search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
-        st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
+        # st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
         st.subheader(fruit_chosen + ' Nutrition Information:')
         fruityvice_response = requests.get(f"https://fruityvice.com/api/fruit/{search_on}")
         fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
